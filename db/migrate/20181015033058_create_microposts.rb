af056c08aa5326226e516675bbb60dba1e286807
exenzomasa <boobyenzo@gmail.com>
2018-10-24 07:06:47 +0000

    heroku 6th

--- db/migrate/20181015033058_create_microposts.rb
+++ db/migrate/20181015033058_create_microposts.rb
@@ -1,12 +1,10 @@
 class CreateMicroposts < ActiveRecord::Migration[5.0]
   def change
-    create_table :favorites do |t|
+    create_table :microposts do |t|
       t.string :content
       t.references :user, foreign_key: true
-      t.references :fav, foreign_key: { to_table: :users }
 
       t.timestamps
-
     end
   end
 end
